e523a6e359d4525cc5387d1191c08fdedd2cdd5d
Taxalottle Syndicate <bthole41@gmail.com>
2026-06-02 11:38:27 -0400

    More cards
    I put some more cards into the game and made the enemies side of the board.

--- hand.gd
+++ hand.gd
@@ -1,6 +1,6 @@
 extends Node2D
 
-var open=true;
+var open=false;
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -8,9 +8,9 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
-	if(get_global_mouse_position().y>850 and not open):
+	if(get_global_mouse_position().y>2500 and not open):
 		open=true
 		$animator.play("open")
-	elif(get_global_mouse_position().y<850 and open):
+	elif(get_global_mouse_position().y<1000 and open):
 		open=false
 		$animator.play("close")
